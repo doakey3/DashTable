def8abd6a8ee22e8a992f30113e492a845cebb50
Low Kian Seong <kianseong@gmail.com>
2015-06-29 01:18:50 +0800

    Update setup.py
    Adding `description` to `setup.py`. This will automatically be picked up by `pypi` and shown as description to search results.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='DashTable',
       version='1.1.2',
       author='Gustav Klopp',
       author_email='gustavk@protonmail.com',
+      description='A library for converting a HTML table into an ASCII table',
       url='https://github.com/gustavklopp/DashTable',
       packages=['DashTable'],
 )
