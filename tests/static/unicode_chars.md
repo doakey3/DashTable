08a846c3506fd15630edff09af8312dbbbf8b4de
doakey3 <github.dmodo@spamgourmet.com>
2017-07-26 03:17:45 -0700

    added unicode test

---
+++ tests/static/unicode_chars.md
@@ -0,0 +1,6 @@
+| ID |         Name         |
+|----|----------------------|
+| x3 | Gilles de la Chambre |
+| x2 |    Jean d'Auriac     |
+| x1 |    Pierre d'Antin    |
+| x4 |     Guy Brantôme     |
--- tests/test.py
+++ tests/test.py
@@ -22,26 +22,25 @@ class TestMatches(unittest.TestCase):
                 
                 rst_name = os.path.splitext(file)[0] + '.rst'
                 rst_path = os.path.join(self.static_path, rst_name)
-                rst_file = open(rst_path, 'r')
+                rst_file = open(rst_path, 'r', encoding='utf-8')
                 rst_text = rst_file.read().rstrip()
                 rst_file.close()
                 
                 try:
                     self.assertEqual(rst, rst_text)
-                except AssertionError:
+                except AssertionError:)
                     print('MATCH ERROR: ' + ntpath.basename(html_path))
                 
                 
                 md_name = os.path.splitext(file)[0] + '.md'
                 md_path = os.path.join(self.static_path, md_name)
-                md_file = open(md_path, 'r')
+                md_file = open(md_path, 'r', encoding='utf-8')
                 md_text = md_file.read().rstrip()
                 md_file.close()
                 
                 try:
                     self.assertEqual(md, md_text)
                 except AssertionError:
-                    
                     print('MATCH ERROR: ' + ntpath.basename(html_path))
         
 if __name__ == '__main__':
