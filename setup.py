ea889c2ec529a88581e449404d6433d9577e8c63
doakey3 <github.dmodo@spamgourmet.com>
2018-02-02 15:14:59 -0700

    version update on readme

--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@ from setuptools import setup
 
 setup(
     name='dashtable',
-    packages=['dashtable'],
-    version='1.4.0',
+    packages=[
+        'dashtable',
+            'dashtable.dashutils',
+            'dashtable.data2md',
+            'dashtable.data2rst',
+            'dashtable.data2simplerst',
+            'dashtable.grid2data',
+            'dashtable.html2data',
+            'dashtable.simple2data'],
+    version='1.4.1',
     description='A library for converting a HTML tables into ASCII tables, rowspan and colspan allowed!',
     long_description=open('README.rst').read(),
     author='doakey3 & gustavklopp',
